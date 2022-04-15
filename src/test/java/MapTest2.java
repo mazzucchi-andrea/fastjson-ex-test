aeb9f2dc713bb0455c2f3c9bdb6b059f669ccb02
mazzucchi-andrea <mazzucchiandrea@gmail.com>
2022-04-15 10:47:55 +0200

    MapTest2 upgrade to parameterized

--- src/test/java/MapTest2.java
+++ src/test/java/MapTest2.java
@@ -1,17 +1,33 @@
-import java.util.Map;
-
-import org.junit.Assert;
-import junit.framework.TestCase;
-
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
-public class MapTest2 extends TestCase {
-    public void test_map () throws Exception {
-        Map<Object, Object> map = JSON.parseObject("{1:\"2\",\"3\":4,'5':6}", new TypeReference<Map<Object, Object>>() {});
-        Assert.assertEquals("2", map.get(1));
-        Assert.assertEquals(4, map.get("3"));
-        Assert.assertEquals(6, map.get("5"));
+import java.util.Map;
+
+@RunWith(Parameterized.class)
+public class MapTest2 {
+    private String jsonStr;
+    private Object[] actualValues;
+    private Object[] expectedValues;
+    public MapTest2() {
+        configure_test_map();
+    }
+    private void configure_test_map() {
+        this.jsonStr = "{1:\"2\",\"3\":4,'5':6}";
+        this.actualValues = new Object[]{1, "3", "5"};
+        this.expectedValues = new Object[]{"2", 4, 6};
+    }
+
+    @Test
+    public void test_map() throws Exception {
+        Map<Object, Object> map = JSON.parseObject(jsonStr, new TypeReference<Map<Object, Object>>() {
+        });
+        Assert.assertEquals(expectedValues[0], map.get(actualValues[0]));
+        Assert.assertEquals(expectedValues[1], map.get(actualValues[1]));
+        Assert.assertEquals(expectedValues[2], map.get(actualValues[2]));
     }
 }
