dcb78e4aeeadf618f278c6ee092afd98b19b5059
mazzucchi-andrea <mazzucchiandrea@gmail.com>
2022-06-01 09:56:13 +0200

    Complaint tests

--- src/test/java/MapTest2.java
+++ src/test/java/MapTest2.java
@@ -1,29 +1,27 @@
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.TypeReference;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import java.util.Map;
 
-@RunWith(Parameterized.class)
 public class MapTest2 {
     private String jsonStr;
     private Object[] actualValues;
     private Object[] expectedValues;
+
     public MapTest2() {
-        configure_test_map();
+        configure();
     }
-    private void configure_test_map() {
+
+    private void configure() {
         this.jsonStr = "{1:\"2\",\"3\":4,'5':6}";
         this.actualValues = new Object[]{1, "3", "5"};
         this.expectedValues = new Object[]{"2", 4, 6};
     }
 
     @Test
-    public void test_map() throws Exception {
+    public void test_map() {
         Map<Object, Object> map = JSON.parseObject(jsonStr, new TypeReference<Map<Object, Object>>() {
         });
         Assert.assertEquals(expectedValues[0], map.get(actualValues[0]));
--- src/test/java/ParserConfigTest.java
+++ src/test/java/ParserConfigTest.java
@@ -1,43 +1,31 @@
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.parser.ParserConfig;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameters;
 
-import java.util.Arrays;
-import java.util.Collection;
-
-@RunWith(Parameterized.class)
 public class ParserConfigTest {
 
     private int expected;
     private String jsonStr;
 
-    public ParserConfigTest(int expected, String jsonStr){
-        this.expected = expected;
-        this.jsonStr = jsonStr;
+    public ParserConfigTest() {
+        configure();
     }
 
-    @Parameters
-    public static Collection<Object> getTestParameters(){
-        return Arrays.asList(new Object[][] {
-                {123, "{\"value\":123}"},
-                {456, "{\"value\":456}"},
-                {789, "{\"value\":789}"}
-        });
+    private void configure() {
+        this.expected = 123;
+        this.jsonStr = "{\"value\":123}";
     }
 
     @Test
-    public void test_0() throws Exception{
+    public void test_0() {
         ParserConfig config = new ParserConfig();
-        config.getDeserializers();
+        Assert.assertNotNull(config.getDeserializers());
+
     }
 
     @Test
-    public void test_1() throws Exception{
+    public void test_1() {
         ParserConfig config = new ParserConfig(Thread.currentThread().getContextClassLoader());
 
         Model model = JSON.parseObject(jsonStr, Model.class, config);
